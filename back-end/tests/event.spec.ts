b51933137e081fb5376083b46290a2c913e45ddf
annaLarousse <a.larousse.riviere@gmail.com>
2022-04-04 18:02:29 -0400

    [BE] Tests file init

---
+++ back-end/tests/event.spec.ts
@@ -0,0 +1,10 @@
+describe('GET / events', () => {
+  it('should respond with status 200 when there is data', () => {});
+  it('should respond with status 404 when there is no data', () => {});
+  it('should return all elements of the data file', () => {});
+});
+
+describe('POST / event', () => {
+  it('should return status 201 when creation completed successfully', () => {});
+  it('should return the id of the new element after creation completed successfully', () => {});
+});
