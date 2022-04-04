f5df14b30a35a8c0ddf4a0479610604497b94645
annaLarousse <a.larousse.riviere@gmail.com>
2022-04-04 17:06:35 -0400

    Init BE - folder created and dependencies added

---
+++ back-end/server.ts
@@ -0,0 +1,15 @@
+import { app } from './app';
+
+const port: number = 8081;
+const allowedUrl: string = `http://localhost:4200`;
+
+app.use(function (_req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', allowedUrl);
+  res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
+app.listen(port, () => {
+  console.log(`server started at http://localhost:${port}`);
+});
