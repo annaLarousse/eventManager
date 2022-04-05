8928731c81c92a0baccf93cb10d50aae1a82b462
annaLarousse <a.larousse.riviere@gmail.com>
2022-04-04 21:34:28 -0400

    [BE] POST api + tests

--- back-end/api/event.ts
+++ back-end/api/event.ts
@@ -1,5 +1,6 @@
 import fs from 'fs';
 import { Express, Request, Response } from 'express';
+import { IEvent } from '../models/event';
 
 const dataFilePath: string = __dirname + '/../data/' + 'events.json';
 module.exports = (app: Express) => {
@@ -11,6 +12,30 @@ module.exports = (app: Express) => {
     else {
       res.status(200);
       res.end(data);
+    }
+  });
+
+  app.post('/event', (req: Request, res: Response) => {
+    const newEvent: IEvent = req.body;
+    if (newEvent?.name?.length > 32) {
+      res.status(400).json('Event name cannot exceed 32 characters');
+    } else {
+      res.contentType('text/plain');
+      let data = getEventsFromJSONFile();
+      const dataLength: number = !data
+        ? 0
+        : Object.keys(JSON.parse(data))?.length;
+      const newId: number = dataLength + 1;
+      data = !data ? {} : JSON.parse(data);
+
+      newEvent.id = newId;
+      (data[newEvent.id] as Object) = newEvent;
+
+      data = JSON.stringify(data, undefined, 2);
+      fs.writeFileSync(dataFilePath, data);
+
+      res.writeHead(201);
+      res.end(newId.toString());
     }
   });
 };
--- back-end/tests/event.spec.ts
+++ back-end/tests/event.spec.ts
@@ -39,6 +39,39 @@ describe('GET / events', () => {
 });
 
 describe('POST / event', () => {
-  it('should return status 201 when creation completed successfully', () => {});
-  it('should return the id of the new element after creation completed successfully', () => {});
+  const newElementInitialValue = {
+    'name': 'Mock event test Post',
+    'description': 'Mock event test post description',
+    'startDate': '2022-04-04T00:0:00.000Z',
+    'endDate': '2022-04-05T00:0:00.000Z',
+  };
+  let newElement: any = { ...newElementInitialValue };
+  afterEach(() => {
+    newElement = { ...newElementInitialValue };
+  });
+
+  it('should return status 201 when creation completed successfully', async () => {
+    (<jest.Mock>fs.readFileSync).mockReturnValueOnce(
+      JSON.stringify(mockEvents)
+    );
+    const res = await request(app).post('/event').send(newElement).then();
+    expect(res.status).toBe(201);
+  });
+
+  it('should return status 400 when name property exceed 32 characters', async () => {
+    (<jest.Mock>fs.readFileSync).mockReturnValueOnce(
+      JSON.stringify(mockEvents)
+    );
+    newElement['name'] = 'This event name is really too long';
+    const res = await request(app).post('/event').send(newElement).then();
+    expect(res.status).toBe(400);
+  });
+
+  it('should return the id of the new element after creation completed successfully', async () => {
+    (<jest.Mock>fs.readFileSync).mockReturnValueOnce(
+      JSON.stringify(mockEvents)
+    );
+    const res = await request(app).post('/event').send(newElement).then();
+    expect(res.text).toBe((Object.keys(mockEvents)?.length + 1).toString());
+  });
 });
