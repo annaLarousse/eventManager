b59c94fdd192f1f781973a24c3db5be0a1088924
annaLarousse <a.larousse.riviere@gmail.com>
2022-04-06 22:16:08 -0400

    [FE] Complete event-add component tests + snapshot

--- front-end/src/app/event/components/event-add/event-add.component.spec.ts
+++ front-end/src/app/event/components/event-add/event-add.component.spec.ts
@@ -2,13 +2,18 @@ import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ReactiveFormsModule } from '@angular/forms';
+import { of } from 'rxjs';
 import { EventService } from '../../services/event.service';
 import { EventAddComponent } from './event-add.component';
 
 describe('EventAddComponent', () => {
   let component: EventAddComponent;
   let fixture: ComponentFixture<EventAddComponent>;
-  let eventServiceMock: any;
+
+  let eventServiceMock = {
+    postEvent: () =>
+      of({ type: 'success', message: 'Event added successfully' }),
+  };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -25,7 +30,23 @@ describe('EventAddComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should display the add new event form', () => {
+    expect(fixture).toMatchSnapshot();
+  });
+
+  describe('AddNewEvent()', () => {
+    it('should call postEvent() of EventService', () => {
+      const spyForm = jest.spyOn(component.form, 'reset');
+      const spyPostEvent = jest.spyOn(eventServiceMock, 'postEvent');
+      expect(spyPostEvent).not.toHaveBeenCalled();
+      component.onAddNewEvent();
+      expect(spyPostEvent).toHaveBeenCalled();
+    });
+
+    it('should reset the form after event added succeffully', () => {
+      const spyForm = jest.spyOn(component.form, 'reset');
+      component.onAddNewEvent();
+      expect(spyForm).toHaveBeenCalled();
+    });
   });
 });
